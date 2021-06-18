8f672f6b19ecf842f369bd507a76f5708e20c8e1
Mikhail <bskfrost3@yandex.ru>
2021-06-18 10:41:24 +0700

    Add SnapKit

--- AppleMusicSwiftUI/View/Modules/Search/SearchView.swift
+++ AppleMusicSwiftUI/View/Modules/Search/SearchView.swift
@@ -7,3 +7,18 @@
 
 import UIKit
 
+class SearchView: UICollectionView {
+
+    lazy var searchBar = UISearchBar()
+
+    lazy var collectionView = UICollectionView()
+
+    func setupHierrarchy() {
+        addSubview(searchBar)
+        addSubview(collectionView)
+    }
+
+    func setupLayout() {
+        
+    }
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+use_frameworks!
+
+target 'AppleMusicSwiftUI' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for AppleMusicSwiftUI
+pod 'SnapKit', '~> 5.0.0'
+end
